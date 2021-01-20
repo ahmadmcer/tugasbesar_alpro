2bbced4ab4a71971a35c3795546acb89dd0802a1
AhmadMCer <ahmadmcer@gmail.com>
2021-01-20 08:00:27 +0700

    Memperbaiki perulangan supaya bisa kembali ke beranda setelah pesan makanan atau lihat pesanan

--- src/com/kelompok3/warteg/Warteg.java
+++ src/com/kelompok3/warteg/Warteg.java
@@ -2,6 +2,14 @@ package com.kelompok3.warteg;
 import java.util.Scanner;
 
 public class Warteg {
+    public static void pesanMakanan() {
+
+    }
+
+    public static void lihatPesanan() {
+
+    }
+
     public static void main(String[] args) {
         // Inisialisasi scanner
         Scanner read = new Scanner(System.in);
@@ -13,39 +21,46 @@ public class Warteg {
         int[] totalPesanan;
         int kode;
         boolean ulangi;
+        boolean kembali;
 
         // Inisialisasi daftar menu dan daftar harga makanan
         // daftarMakanan = {{}};
         // daftarHarga = {};
 
-        // Tampilkan Beranda
-        System.out.println("WARTEG ONLINE");
-        System.out.println("1. Pesan Makanan");
-        System.out.println("2. Lihat Pesanan");
-        System.out.println("0. Tutup Aplikasi");
-
-        // Masukkan kode
         do {
-            ulangi = true;
+            kembali = true;
 
-            System.out.print("> ");
-            kode = read.nextInt();
+            // Tampilkan Beranda
+            System.out.println("WARTEG ONLINE");
+            System.out.println("1. Pesan Makanan");
+            System.out.println("2. Lihat Pesanan");
+            System.out.println("0. Tutup Aplikasi");
 
-            //gunakan switch case pada bagian bagan.
+            // Masukkan kode
+            do {
+                ulangi = true;
 
-            switch (kode) {
-                case 1:
-                    // pesan;
-                    ulangi = false;
-                    break;
-                case 2:
-                    // keranjang;
-                    ulangi = false;
-                    break;
-                case 0:
-                    ulangi = false;
-                    break;
-            }
-        } while (ulangi);
+                System.out.print("> ");
+                kode = read.nextInt();
+
+                switch (kode) {
+                    case 1:
+                        // pesan;
+                        ulangi = false;
+                        break;
+                    case 2:
+                        // keranjang;
+                        ulangi = false;
+                        break;
+                    case 0:
+                        ulangi = false;
+                        kembali = false;
+                        break;
+                    default:
+                        System.out.println("\n[EROR] Maaf, kode tidak dikenali!");
+                        break;
+                }
+            } while (ulangi);
+        } while (kembali);
     }
 }
